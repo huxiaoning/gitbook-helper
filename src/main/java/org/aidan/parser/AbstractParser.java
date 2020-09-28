7fcad3bbe5e717b4411a7246c7ea47d1c07c762a
huxiaoning <huxiaoningsworld@sina.com>
2020-09-29 01:45:38 +0800

    UP

---
+++ src/main/java/org/aidan/parser/AbstractParser.java
@@ -0,0 +1,9 @@
+package org.aidan.parser;
+
+/**
+ * 解析器抽象
+ *
+ * @author huxiaoning
+ */
+public class AbstractParser {
+}
--- src/test/java/org/aidan/AppTest.java
+++ src/test/java/org/aidan/AppTest.java
@@ -1,20 +1,35 @@
 package org.aidan;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test() {
+        String workDir = "C:\\Users\\huxiaoning\\Documents\\mybook\\env";
+        SummaryGenerator summaryGenerator = new SummaryGenerator(workDir);
+        String result = summaryGenerator.generate();
+        Assert.assertEquals("# Summary\n" +
+                "\n" +
+                "* [env](README.md)\n" +
+                "* [Git](Git/README.md)\n" +
+                "\t* [01 Git安装配置](Git/01 Git安装配置.md)\n" +
+                "\t* [02 Git忽略不想提交的配置文件](Git/02 Git忽略不想提交的配置文件.md)\n" +
+                "\t* [03 Gitbook安装配置](Git/03 Gitbook安装配置.md)\n" +
+                "* [Java安装配置](Java安装配置/README.md)\n" +
+                "\t* [Java安装配置之Centos](Java安装配置/Java安装配置之Centos.md)\n" +
+                "\t* [Java安装配置之Windows](Java安装配置/Java安装配置之Windows.md)\n" +
+                "* [Maven安装配置](Maven安装配置/README.md)\n" +
+                "\t* [Maven安装配置Windows](Maven安装配置/Maven安装配置Windows.md)\n" +
+                "* [test1](test1/README.md)\n" +
+                "\t* [01 a](test1/01 a.md)\n" +
+                "\t* [02 b](test1/02 b.md)\n" +
+                "\t* [test2](test1/test2/README.md)\n" +
+                "\t\t* [01 xxx](test1/test2/01 xxx.md)\n" +
+                "\t\t* [02 yyyy](test1/test2/02 yyyy.md)\n", result);
     }
 }
